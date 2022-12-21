8b8ce057b6c86fcfeb69d77de6019092412bb5e3
revol-xut <revol-xut@protonmail.com>
2022-12-21 02:17:46 +0100

    setting up systemd-nspawn for watch-me-senpai

--- flake.nix
+++ flake.nix
@@ -45,7 +45,12 @@
               privateNetwork = true;
               hostAddress = "192.168.100.10";
               localAddress = "192.168.100.11";
-              config = { config, pkgs, ... }: { };
+              config = { config, pkgs, ... }: {
+                imports = [
+                  dump-dvb."x86_64-linux".watch-me-senpai
+                  ./modules/watch-me-senpai.nix
+                ];
+              };
             };
           }
         ];
--- modules/watch-me-senpai.nix
+++ modules/watch-me-senpai.nix
@@ -1,39 +1,11 @@
 { pkgs, config, lib, ... }: {
-  networking.nat = {
+  system.stateVersion = "22.11";
+
+  networking.firewall = {
     enable = true;
-    internalInterfaces = [ "ve-+" ];
-    externalInterface = "ens3";
-    # Lazy IPv6 connectivity for the container
-    enableIPv6 = true;
+    allowedTCPPorts = [ 80 ];
   };
 
-  containers.nextcloud = {
-    autoStart = true;
-    privateNetwork = true;
-    hostAddress = "192.168.100.10";
-    localAddress = "192.168.100.11";
-    hostAddress6 = "fc00::1";
-    localAddress6 = "fc00::2";
-    config = { config, pkgs, ... }: {
+  environment.etc."resolv.conf".text = "nameserver 8.8.8.8";
 
-      services.nextcloud = {
-        enable = true;
-        package = pkgs.nextcloud24;
-        hostName = "localhost";
-        config.adminpassFile = pkgs.writeText "adminpass" "test123"; # DON'T DO THIS IN PRODUCTION - the password file will be world-readable in the Nix Store!
-      };
-
-      system.stateVersion = "22.05";
-
-      networking.firewall = {
-        enable = true;
-        allowedTCPPorts = [ 80 ];
-      };
-
-      # Manually configure nameserver. Using resolved inside the container seems to fail
-      # currently
-      environment.etc."resolv.conf".text = "nameserver 8.8.8.8";
-
-    };
-  };
 }
